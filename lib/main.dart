1e55137b1e42b8991c1a9ed91c43da076d60d6a5
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2022-11-29 19:11:35 +0900

    finish step3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_app/poke_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,55 +14,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TopPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(32),
-                child: Image.network(
-                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-                  height: 100,
-                  width: 100,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text(
-                  'No.25',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const Text(
-            "pikachu",
-            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-          ),
-          const Chip(
-            label: Text("electric"),
-            backgroundColor: Colors.yellow,
-          )
-        ],
-      )),
+      body: ListView.builder(
+          itemCount: 10000,
+          itemBuilder: (context, index) => PokeListItem(index: index)),
     );
   }
 }
+
